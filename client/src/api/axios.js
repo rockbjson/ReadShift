d75b0865d6c875e8e56259a8e9c29ee3fc09c6f2
rockbjson <tanisidas.21@gmail.com>
2026-07-17 23:33:05 +0400

    update API base URL to Railway deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://readshift-production.up.railway.app/api',
 });
 
 // Attach JWT token to every request automatically
