c5c40dcd0b4547ec67b81f006ab988f321c18198
rockbjson <tanisidas.21@gmail.com>
2026-07-18 00:02:39 +0400

    fix environment variable loading for Railway production

--- server/db.js
+++ server/db.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const authRoutes = require('./routes/auth');
 const articleRoutes = require('./routes/articles');
